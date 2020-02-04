9844beaa8fc2345e146902a00f3a4e77493d5587
Blinko26 <bruno.viellard@gmail.com>
2020-02-04 09:06:59 +0100

    base

---
+++ enemy.py
@@ -0,0 +1,62 @@
+import pygame
+pygame.init()
+
+class enemy(object):
+
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.path = [x, end]
+        self.walkCount = 0
+        self.vel = 3
+
+        self.walkRight = [pygame.image.load('sprite/visonDroit.png')]
+        self.walkLeft = [pygame.image.load('sprite/visonGauche.png')]
+
+        self.hitbox = (self.x, self.y, 50, 50)
+        self.health = 10
+        self.visible = True
+
+    def draw(self, windows):
+        if self.visible:
+            self.move()
+            if self.walkCount + 1 >= 33:
+                self.walkCount = 0
+            if self.vel > 0:
+                windows.blit(self.walkRight[0], (self.x, self.y))
+                self.walkCount += 1
+            else:
+                windows.blit(self.walkLeft[0], (self.x, self.y))
+                self.walkCount += 1
+            self.hitbox = (self.x, self.y, 60, 30)
+            pygame.draw.rect(windows, (255, 0, 0), self.hitbox, 2)
+
+            pygame.draw.rect(windows, (255, 0, 0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10))
+            pygame.draw.rect(windows, (0, 128, 0),
+                             (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (10 - self.health)), 10))
+            self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+
+    def move(self):
+        if self.vel > 0:
+            if self.x < self.path[1] + self.vel:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.x += self.vel
+                self.walkCount = 0
+        else:
+            if self.x > self.path[0] - self.vel:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.x += self.vel
+                self.walkCount = 0
+    def hit(self):
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.visible = False
+        print('hit')
--- player.py
+++ player.py
@@ -1,17 +1,39 @@
 import pygame
+pygame.init()
 
-
-
-class Player():
-    def __init__(self):
-        self.health = 100
-        self.maxHealth= 100
-        self.velocity = 5
-        self.attack = 10
-        self.image = pygame.image.load('./testAnim/player01.png')
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.left = False
+        self.right = False
         self.all_projectile = pygame.sprite.Group()
+        self.walkCount = 0
+        self.jumpCount = 8
+        self.stranding = True
 
+        self.hitbox = (self.x, self.y, 50, 50)
 
+        self.walkRight = [pygame.image.load('sprite/petitTrollDroit.png')]
+        self.walkLeft = [pygame.image.load('sprite/petitTrollGauche.png')]
+        self.char = pygame.image.load('sprite/petitTroll.png')
 
-    def launchProjectile(self,projectile):
-        self.all_projectile.add(projectile) # ajoute un projectile a la liste
+    def draw(self, windows):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if not (self.standing):
+            if self.left:
+                windows.blit(self.walkLeft[0], (self.x, self.y))
+                self.walkCount += 1
+            elif self.right:
+                windows.blit(self.walkRight[0], (self.x, self.y))
+                self.walkCount += 1
+        else:
+            windows.blit(self.char, (self.x, self.y))
+        self.hitbox = (self.x +10, self.y+5, 30, 35)  # NEW
+        pygame.draw.rect(windows, (255, 0, 0), self.hitbox, 2)
